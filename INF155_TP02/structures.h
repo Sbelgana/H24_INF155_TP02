f1040bed90f95952a963bf8260d70fb79bd1b859
Sbelgana <44445774+Sbelgana@users.noreply.github.com>
2024-03-21 12:48:25 -0400

    Update structures.h

--- INF155_TP02/structures.h
+++ INF155_TP02/structures.h
@@ -12,7 +12,7 @@
 #define NOM_FICHIER_MATCHES "..//matchs2022.txt"
 
 
-typedef struct {
+typedef struct t_equipe{
     char id[10];
     char nom[20];
     char conf[20];
@@ -20,42 +20,42 @@ typedef struct {
     int vic, def, nul;
     int buts_p, buts_c, diff_buts;
     int pts, clas, matche_jouer;
-} Equipe;
+};
 
-typedef struct {
+typedef struct t_groupe{
     char nom;
     Equipe* eqp;
     int nb_eqp;
     int cap;
-} Groupe;
+} ;
 
 typedef struct {
     int buts_eq1, buts_eq2;
 }Buts;
 
-typedef struct {
+typedef struct t_eliminatoire{
     Equipe rencontre[2];
 }Eliminatoire;
 
-typedef struct {
+typedef struct t_wcup{
     Groupe grp[8];
     Eliminatoire tour_16[8];
     Eliminatoire tour_8[4];
     Eliminatoire tour_4[2];
     Eliminatoire finale;
-} W_CUP;
+};
 
-typedef struct{
+typedef struct t_matche{
     char id_vis[10], id_dom[10];
     int clas_vis, clas_dom;
     int buts_eq1, buts_eq2;
     int pts_vis, pts_dom;
 }Matche;
 
-typedef struct{
+typedef struct t_liste_matches{
     Matche* matches;
     int nb_matche;
     int cap;
-}Liste_matches;
+};
 
 #endif //INF155_TP02_STRUCTURES_H
