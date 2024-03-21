c855c7c989f39eb75bc34f46b9819a1a40ef1903
Sbelgana <44445774+Sbelgana@users.noreply.github.com>
2024-03-21 12:51:58 -0400

    Update phase_elimin.h

--- INF155_TP02/phase_elimin.h
+++ INF155_TP02/phase_elimin.h
@@ -3,10 +3,10 @@
 
 #include "sim_phase_grp.h"
 
-void equipes_qualifiees(W_CUP *wc);
+void equipes_qualifiees(t_wcup *wc);
 Equipe* jouer_match_eliminatoire(Equipe *eq1, Equipe *eq2);
-void simuler_tour(W_CUP *wc, Eliminatoire *tour_actuel, int nb_matchs, Eliminatoire *prochain_tour) ;
-void tour_eliminatoires(W_CUP *wc);
+void simuler_tour(t_wcup *wc, t_eliminatoire *tour_actuel, int nb_matchs, t_eliminatoire *prochain_tour) ;
+void tour_eliminatoires(t_wcup *wc);
 
 void test_equipes_qualifiees();
 void test_jouer_match_eliminatoire();
